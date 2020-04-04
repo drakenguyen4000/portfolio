857a881d2183d1a416762b63fbae8c998570f0c1
Drake Nguyen <drakeknguyen@yahoo.com>
2020-04-04 16:53:35 -0700

    update portfolio & added menu webpage

---
+++ 4-projects/menu/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(".burger-nav").on("click", function(){
+        $(".container nav ul").toggleClass("open");
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
