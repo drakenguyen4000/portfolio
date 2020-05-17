ead997a8d160a03eaab523aa6223946e8b756616
Drake Nguyen <drakeknguyen@yahoo.com>
2020-05-17 13:49:07 -0700

    added animation to name

--- 8-resources/script.js
+++ 8-resources/script.js
@@ -1,10 +1,42 @@
-$(".js--wp-1, .js--wp-2, .js--wp-3").waypoint(
+$(".js--wp-1").waypoint(
   function(direction) {
-    $(".js--wp-1, .js--wp-2, .js--wp-3").addClass("animated fadeIn");
+    $(".js--wp-1").addClass("animated fadeIn");
+  },
+  {
+    offset: "50%"
+  }
+);
+$(".js--wp-2").waypoint(
+  function(direction) {
+    $(".js--wp-2").addClass("animated fadeIn");
+  },
+  {
+    offset: "50%"
+  }
+);
+$(".js--wp-3").waypoint(
+  function(direction) {
+    $(".js--wp-3").addClass("animated fadeIn");
   },
   {
     offset: "50%"
   }
 );
 
+$(".js--wp-n-1, .js--wp-n-2, .js--wp-n-3, .js--wp-n-4, .js--wp-n-5, .js--wp-n-6, .js--wp-n-7, .js--wp-n-8, .js--wp-n-9, .js--wp-n-10, .js--wp-n-11").waypoint(
+  function(direction) {
+    $(".js--wp-n-1, .js--wp-n-2, .js--wp-n-3, .js--wp-n-4, .js--wp-n-5, .js--wp-n-6, .js--wp-n-7, .js--wp-n-8, .js--wp-n-9, .js--wp-n-10, .js--wp-n-11").addClass("animated fadeIn");
+  },
+  {
+    offset: "100%"
+  }
+);
 
+$(".js--wp-n-12").waypoint(
+  function(direction) {
+    $(".js--wp-n-12").addClass("animated fadeIn");
+  },
+  {
+    offset: "100%"
+  }
+);
